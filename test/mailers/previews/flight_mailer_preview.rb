fe9b31c5e987fb1d977c6c761dfb5aae1fc65e09
Kayode Adeniyi <kayode.adeniyi@andela.co>
2015-09-18 21:43:00 +0100

    Removed minitest from project

--- test/mailers/previews/flight_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/flight_mailer
-class FlightMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/flight_mailer/flight_details
-  def flight_details
-    FlightMailer.flight_details
-  end
-
-end
